3cc430a968d0579bbdc688f39d79d54942d98fd9
imark114 <imark114@gmail.com>
2025-01-26 16:58:17 +0600

    custom schema create and insert the django models into them

--- postgres/scripts/db_script.sh
+++ postgres/scripts/db_script.sh
@@ -5,3 +5,9 @@ export PGUSER="postgres"
 psql -c "CREATE DATABASE inventory"
 
 psql inventory -c "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";"
+
+psql inventory -c "CREATE SCHEMA inventory_schema"
+
+psql inventory -c "CREATE SCHEMA inventory_management_schema"
+
+psql inventory -c "ALTER DATABASE inventory SET search_path TO inventory_schema, inventory_management_schema"
